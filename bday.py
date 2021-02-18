0fe5a2cfd49c0debf38da284d6584fbd41ae8ecb
timscheuermann <t.scheuermann2000@gmail.com>
2021-02-19 00:40:31 +0100

    init

---
+++ bday.py
@@ -0,0 +1,16 @@
+from getpass import getpass
+from webdav3.client import Client
+#from webdav3.client import Client
+
+option = {
+    'webdav_hostname': "https://timscheuermann.ddns.net/remote.php/dav/calendars/admin/geburtstage/",
+    'webdav_login': "admin",
+    'webdav_password': "NULL"
+}
+
+if(option["webdav_password"]=="NULL"):
+    option["webdav_password"] = getpass("Please enter WebDav password: ")
+
+client = Client(option)
+client.verify=True
+print(client.list())
