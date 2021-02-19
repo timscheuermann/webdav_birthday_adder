5a2a76a8d28f4ee712a426f23f6bd7e3197d40fe
timscheuermann <t.scheuermann2000@gmail.com>
2021-02-19 01:00:42 +0100

    test3

--- bday.py
+++ bday.py
@@ -2,7 +2,7 @@ from getpass import getpass
 from webdav3.client import Client
 #from webdav3.client import Client
 #test
-#Testttrr
+#Test3
 
 option = {
     'webdav_hostname': "https://timscheuermann.ddns.net/remote.php/dav/calendars/admin/geburtstage/",
