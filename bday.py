fd8599d54fb050c7c180747839dcbb279efd3cf8
timscheuermann <t.scheuermann2000@gmail.com>
2021-02-21 21:48:55 +0100

    added check_date() for valid input date

--- bday.py
+++ bday.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import date, datetime
+import datetime
 from getpass import getpass
 import caldav
 
@@ -16,14 +17,27 @@ VERSION:2.0
 PRODID:-//Example Corp.//CalDAV Client//EN
 BEGIN:VEVENT
 UID:1234567890
-DTSTAMP:20100510T182145Z
-DTSTART:20100512T170000Z
-DTEND:20100512T180000Z
-SUMMARY:This is an event
+DTSTAMP:20100610T182145Z
+DTSTART:20100612T170000Z
+DTEND:20100612T180000Z
+SUMMARY:Test_Timmy
 END:VEVENT
 END:VCALENDAR
 """
 
+def check_date(day, month, year):
+    isValidDate = True
+    try:
+        datetime.datetime(int(year),int(month),int(day))
+    except ValueError:
+        isValidDate = False
+    if(isValidDate):
+        print("Input date is valid")
+    else :
+        print("Input date is not valid")
+
+check_date(day=29,month=2,year=2021)
+exit()
 client = caldav.DAVClient(**(caldav_servers[0]))
 principal = client.principal()
 calendars = principal.calendars()
@@ -34,8 +48,15 @@ if len(calendars) > 0:
     event = calendar.add_event(vcal)
     print("Event" + str(event) + "created")
 
-    print("Looking for events in 2010-05")
-    results = calendar.date_search(datetime(2010, 5, 1), datetime(2010, 6, 1))
+    print("Looking for events in 2010-06")
+    results = calendar.date_search(datetime(2010, 5, 1), datetime(2010, 7, 1))
 
     for event in results:
         print("Found" + str(event))
+
+
+
+
+
+        
+check_date(1,11,2000)
